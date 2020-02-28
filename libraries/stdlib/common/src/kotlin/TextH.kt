a213c2fe107b909c14f47dcf33aff3ed1222ee5e
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2020-02-28 18:04:15 +0300

    Mark all necessary utils for correct interpretation of unsigned numbers

--- libraries/stdlib/common/src/kotlin/TextH.kt
+++ libraries/stdlib/common/src/kotlin/TextH.kt
@@ -376,6 +376,7 @@ expect fun String.toFloatOrNull(): Float?
  * @throws IllegalArgumentException when [radix] is not a valid radix for number to string conversion.
  */
 @SinceKotlin("1.2")
+@CompileTimeCalculation
 expect fun Byte.toString(radix: Int): String
 
 /**
@@ -384,6 +385,7 @@ expect fun Byte.toString(radix: Int): String
  * @throws IllegalArgumentException when [radix] is not a valid radix for number to string conversion.
  */
 @SinceKotlin("1.2")
+@CompileTimeCalculation
 expect fun Short.toString(radix: Int): String
 
 /**
@@ -392,6 +394,7 @@ expect fun Short.toString(radix: Int): String
  * @throws IllegalArgumentException when [radix] is not a valid radix for number to string conversion.
  */
 @SinceKotlin("1.2")
+@CompileTimeCalculation
 expect fun Int.toString(radix: Int): String
 
 /**
@@ -400,6 +403,7 @@ expect fun Int.toString(radix: Int): String
  * @throws IllegalArgumentException when [radix] is not a valid radix for number to string conversion.
  */
 @SinceKotlin("1.2")
+@CompileTimeCalculation
 expect fun Long.toString(radix: Int): String
 
 @PublishedApi
