83089e38e9a16897fe9b018cdff8b73edd4e0370
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-04-14 10:51:33 +0300

    FIR: Report diagnostics introduced during call completion

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirOldFrontendDiagnosticsTestGenerated.java
@@ -13035,6 +13035,12 @@ public class FirOldFrontendDiagnosticsTestGenerated extends AbstractFirDiagnosti
                 }
 
                 @Test
+                @TestMetadata("errorAfterCompletion.kt")
+                public void testErrorAfterCompletion() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/completion/errorAfterCompletion.kt");
+                }
+
+                @Test
                 @TestMetadata("flexibleType.kt")
                 public void testFlexibleType() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/completion/flexibleType.kt");
