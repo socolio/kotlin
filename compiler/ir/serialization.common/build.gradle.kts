dcf03692a2529f0221aaf74ab8a2ec78a8bfb7d5
Alexander Udalov <alexander.udalov@jetbrains.com>
2021-04-22 20:24:16 +0200

    Remove dependency of ir.serialization.common on frontend

--- compiler/ir/serialization.common/build.gradle.kts
+++ compiler/ir/serialization.common/build.gradle.kts
@@ -4,12 +4,13 @@ plugins {
 }
 
 dependencies {
-    compile(project(":compiler:frontend"))
     compile(project(":compiler:ir.tree"))
     compile(project(":compiler:serialization"))
     compile(project(":kotlin-util-klib"))
     compile(project(":kotlin-util-klib-metadata"))
     compile(project(":compiler:util"))
+    implementation(project(":core:compiler.common.jvm"))
+    implementation(project(":compiler:psi"))
     compileOnly(project(":kotlin-reflect-api"))
 
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
