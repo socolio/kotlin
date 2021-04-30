ea739fbba4cbdb656a6dfcca251a8db36f99a30c
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2021-04-30 18:50:22 +0300

    Add workaround for KT-35001
    Just suppress the warning.
    The accident it describes is generally harmless and totally expected.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/linkModules.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/linkModules.kt
@@ -8,7 +8,24 @@ package org.jetbrains.kotlin.backend.konan.llvm
 import llvm.*
 import org.jetbrains.kotlin.backend.konan.Context
 
-internal fun llvmLinkModules2(context: Context, dest: LLVMModuleRef, src: LLVMModuleRef): LLVMBool =
-        withLlvmDiagnosticHandler(DefaultLlvmDiagnosticHandler(context)) {
-            LLVMLinkModules2(dest, src)
+internal fun llvmLinkModules2(context: Context, dest: LLVMModuleRef, src: LLVMModuleRef): LLVMBool {
+    val diagnosticHandler = object : DefaultLlvmDiagnosticHandler(context) {
+        override fun isIgnored(diagnostic: LlvmDiagnostic): Boolean {
+            if (super.isIgnored(diagnostic)) return true
+
+            // Workaround https://youtrack.jetbrains.com/issue/KT-35001.
+            // Note: SDK version mismatch is generally harmless.
+            // Also it is expected: LLVM bitcode can be built in different environments with different SDK versions,
+            // and then linked by the compiler altogether.
+            // Just ignore such warnings for now:
+            if (diagnostic.severity == LlvmDiagnostic.Severity.WARNING &&
+                    diagnostic.message.startsWith("linking module flags 'SDK Version': IDs have conflicting values")) return true
+
+            return false
         }
+    }
+
+    return withLlvmDiagnosticHandler(diagnosticHandler) {
+        LLVMLinkModules2(dest, src)
+    }
+}
